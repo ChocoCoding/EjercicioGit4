13fd37b13b394de4811dcc5433538599523d1b43
Gonzalo Campos Dominguez <gonzalo.dominguez.cd@gmail.com>
2022-12-04 12:07:48 +0100

    diseñamos las pruebas de esta funcionalidad

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Empleado p = new Empleado("Pepe Pérez", new Date(90, Calendar.FEBRUARY, 1));
         System.out.println(p);
+
+
+        p.nombre = "Manolo"; // Cambio de nombre. No se debe permitir
+        p.fechaContratacion = new Date(99, Calendar.FEBRUARY, 1); // Cambio de fecha. No se debe permitir
     }
 }
